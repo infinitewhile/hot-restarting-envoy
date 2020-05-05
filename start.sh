e619144ee50bd281ba3ec0b359ff614efc2f969c
InfiniteWhile <github@infinitewhile.com>
2020-05-05 21:08:06 +0100

    Fix new line output in start script

--- start.sh
+++ start.sh
@@ -23,8 +23,8 @@ docker run \
 	-v $(pwd)/proxy_config.v2.yaml:/etc/envoy/envoy.yaml \
 	hot-restarting-envoy:v1.14.1
 
-echo "\nEnvoy container is running. Open http://localhost:10000"
+echo -e "\nEnvoy container is running. Open http://localhost:10000"
 
 # Watch for config file changes and reload
-echo "\nStarting to watch proxy_config.v2.yaml for changes..."
+echo -e "\nStarting to watch proxy_config.v2.yaml for changes..."
 fswatch -0 proxy_config.v2.yaml | xargs -0 -I {} ./reload-config.sh
