691168ef15d6e5625c62d0d35d16cbacee967936
InfiniteWhile <github@infinitewhile.com>
2020-05-05 21:00:27 +0100

    Remove --base-id option from the start command

--- docker-bin/start-envoy.sh
+++ docker-bin/start-envoy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-envoy -c /etc/envoy/envoy.yaml --restart-epoch $RESTART_EPOCH --base-id 0 --drain-time-s 0 --parent-shutdown-time-s 0
+envoy -c /etc/envoy/envoy.yaml --restart-epoch $RESTART_EPOCH --drain-time-s 0 --parent-shutdown-time-s 0
